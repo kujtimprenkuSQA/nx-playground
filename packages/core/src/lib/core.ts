975ede1bc50992a59728020a8b4f4dd615a926e5
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-08-25 15:36:54 +0200

    Import a logMessage from core to ui package.

--- packages/core/src/lib/core.ts
+++ packages/core/src/lib/core.ts
@@ -1,3 +1,6 @@
 export function core(): string {
   return 'core';
 }
+export const logMessage = (message: string) => {
+  console.log(message);
+}
--- packages/ui/src/lib/ui.ts
+++ packages/ui/src/lib/ui.ts
@@ -1,3 +1,7 @@
+import { logMessage} from "@nx-playground/core";
+
 export function ui(): string {
   return 'ui';
 }
+
+logMessage("Test");
