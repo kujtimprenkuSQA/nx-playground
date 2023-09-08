104467a3051c111dbaf39d4ccfc6b1c3d1eb1235
kujtimprenkuSQA <95851345+kujtimprenkuSQA@users.noreply.github.com>
2023-09-08 18:19:08 +0200

    Revert "test: I am just testing"

--- .github/actions/pr-title.js
+++ .github/actions/pr-title.js
@@ -42,12 +42,7 @@ module.exports.update = async function ({
 
     title = title.replace(/(\n)?(BREAKING CHANGE).+/g, '');
 
-<<<<<<< HEAD
-    console.log({title})
-=======
     console.log(title)
-    console.log(title)
->>>>>>> dacc496 (add new log.)
 
     if(match) {
         const [, reason] = match.split(':');
