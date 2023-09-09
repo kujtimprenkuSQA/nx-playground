8d9167829affe8e25ceee6fd009d1c1e033ec0e6
kujtimprenkuSQA <95851345+kujtimprenkuSQA@users.noreply.github.com>
2023-09-09 23:19:38 +0200

    feat: Cleanup log messages (#61)
    * Cleanup log messages.
    
    * Add semi-colon.

--- packages/modal-ui-js/src/lib/modal-ui.js.ts
+++ packages/modal-ui-js/src/lib/modal-ui.js.ts
@@ -1,11 +1,7 @@
 import {logMessageUppercase} from "@nx-playground/core";
 
 export const modalUiJs = () => {
-    return "modal-ui-js"
+    return "modal-ui-js";
 }
 
 logMessageUppercase("Test");
-logMessageUppercase("First log message");
-logMessageUppercase("Second log message");
-logMessageUppercase("Third log message");
-logMessageUppercase("Fourth log message");
