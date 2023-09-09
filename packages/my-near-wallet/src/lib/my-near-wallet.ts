d6214dd377b45c7d962ce82ea2b563f20246b3a6
kujtimprenkuSQA <95851345+kujtimprenkuSQA@users.noreply.github.com>
2023-09-09 23:15:36 +0200

    fix: Remove few log messages (#60)
    * Remove 4th log message.
    
    * Remove 3rd log message.
    
    * Remove 2nd log message.

--- packages/my-near-wallet/src/lib/my-near-wallet.ts
+++ packages/my-near-wallet/src/lib/my-near-wallet.ts
@@ -8,7 +8,4 @@ import { logMessageUppercase } from "@nx-playground/core";
 
 logMessageUppercase(walletUtils());
 
-logMessageUppercase("Test my-near-wallet 1")
-logMessageUppercase("Test my-near-wallet 2")
-logMessageUppercase("Test my-near-wallet 3")
-logMessageUppercase("Test my-near-wallet 4")
+logMessageUppercase("Test my-near-wallet 1");
