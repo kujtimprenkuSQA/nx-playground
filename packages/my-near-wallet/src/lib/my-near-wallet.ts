1b6bcf1e536b50c983fb4690e7d20b9369ba3351
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-09-09 23:12:08 +0200

    Remove 4th log message.

--- packages/my-near-wallet/src/lib/my-near-wallet.ts
+++ packages/my-near-wallet/src/lib/my-near-wallet.ts
@@ -11,4 +11,3 @@ logMessageUppercase(walletUtils());
 logMessageUppercase("Test my-near-wallet 1")
 logMessageUppercase("Test my-near-wallet 2")
 logMessageUppercase("Test my-near-wallet 3")
-logMessageUppercase("Test my-near-wallet 4")
