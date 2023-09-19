eaf7bb8f73d36be5ba5afef50ec5684c1c8e669d
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-09-19 22:29:34 +0200

    Add docs for the lowercase  message.

--- packages/core/src/lib/core.ts
+++ packages/core/src/lib/core.ts
@@ -9,7 +9,10 @@ export const core = () => {
 export const logMessageUppercase = (message: string) => {
     console.log(`Uppercase: ${message.toUpperCase()}`);
 }
-
+/**
+ * Logs the message and converts it to lowercase.
+ * @param message
+ */
 export const logMessageLowercase = (message: string) => {
     console.log(`Lowercase: ${message.toUpperCase()}`);
 }
